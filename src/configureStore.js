28862910da3011c63d27dab6696fa02012005bac
Wan <wan@lizard-apps.com>
2019-12-22 01:31:58 +0800

    add redux-thunk

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const middlewares= [ReduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+
+export default createStoreWithMiddleware(rootReducer, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
